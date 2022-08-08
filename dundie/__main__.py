515dcaf8b2d8ee624fe8c378a74461f9db5a4f5f
Felipe Mendes <felipemendesdev@gmail.com>
2022-08-08 19:53:34 -0300

    receiving params and validating

--- dundie/__main__.py
+++ dundie/__main__.py
@@ -1,5 +1,36 @@
+import argparse
+
+def load(filepath):
+    with open(filepath) as file_:
+        for line in file_:
+            print(line)
 
 
 def main():
-    print("Entry point")
+    parser = argparse.ArgumentParser(
+        description="Dunder Mifflin Rewards CLI",
+        epilog="Enjoy and use with cautios.",
+    )
+
+    parser.add_argument(
+        "subcommand",
+        type=str,
+        help="The subcommand to run",
+        choices=("load","show","send"),
+        default="help"
+    )
+    parser.add_argument(
+        "filepath",
+        type=str,
+        help="File path to load",
+        default=None,
+    )
+
+    args = parser.parse_args()
+
+    globals()[args.subcommand](args.filepath)
+  
     
+
+if __name__ == "__main__":
+    main()
