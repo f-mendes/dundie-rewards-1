d1c0379aa2111e733d5f2ebcaab828995d7b6bd0
Felipe Mendes <felipemendesdev@gmail.com>
2022-08-07 23:22:47 -0300

    Set up project initial organization and metada
    - add setup.py to turn installable
    - add __main__ for executable
    - add __init__ for package
    
    closes #8

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Reward Point Systen for Dunder Mifflin",
+    author="Felipe Mendes",
+    packages=find_packages(),
+)
