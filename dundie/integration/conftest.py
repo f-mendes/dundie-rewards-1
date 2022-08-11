95ac1c62253f738a9d17d68960be318364b369cc
Felipe Mendes <felipemendesdev@gmail.com>
2022-08-10 22:41:33 -0300

    corrected warning in test

--- dundie/integration/conftest.py
+++ dundie/integration/conftest.py
@@ -5,7 +5,11 @@ medium: Medium Priority
 low: Low Priority
 """
 
+# Throwing warnning in the tests
+#def pytest_configure(config):
+#   map(lambda line: config.addinivalue_line('markers', line), MARKER.split("\n"))
 
 def pytest_configure(config):
-    map(lambda line: config.addinivalue_line('markers', line), MARKER.split("\n"))
+    for line in MARKER.split("\n"):
+        config.addinivalue_line('markers', line)
     
